b9b9b8b504b54b4ac54adc316bb031277c88b4f7
huynh0801 <huynh080104@gmail.com>
2025-12-20 14:46:06 +0700

    sửa logic gọi API và hiển thị

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./styles/Header.js";
+import Footer from "./styles/Footer.js";
 import NewsDetail from "./components/NewsDetail";
 import NewsFeed from "./components/NewsFeed";
 import "./styles/global.css";
